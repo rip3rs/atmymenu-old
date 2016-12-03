72f3f6f6e28447ca6430d20155e1aa2532d7a167
Joseph Briggs <jbriggsr3@gmail.com>
2016-12-03 22:12:44 +0000

    Added Mongodb and parser dependencies... added form for testing in index.html, made the ability to save into the db.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
-var express = require('express'),
-    app = express()
-    port = 3000;
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+var db
+
+MongoClient.connect('mongodb://127.0.0.1', (err, database) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    db = database;
+
+    app.listen(port, () => {
+        console.log('listening on ' + port);
+    });
+});
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.post('/quotes', (req, res) => {
-  console.log('Hellooooooooooooooooo!')
-})
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) {
+            return console.log(err)
+        }
 
-app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
+        console.log('saved to database');
+        console.log(db);
+        res.redirect('/');
+    });
 });
